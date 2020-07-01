3f6b3cc6c74ada1f9005a61211621e33fe57fde1
Daniel Naschberger <daniel@naschi.at>
2020-07-01 19:40:40 +0200

    download spatialMOS data

--- GFSV2_bulk.sh
+++ GFSV2_bulk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose run --user 1000:1000 pygfsv2 python GFSV2_bulk.py --config spatialMOS_tmp2m.conf
+docker-compose run --user 1000:1000 pygfsv2 python GFSV2_bulk.py --config spatialMOS.conf
